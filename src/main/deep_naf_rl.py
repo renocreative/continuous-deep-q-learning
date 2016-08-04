909641607a95db59f5b402f290d8bb5569837de1
Renaud <renaud.bougues@outlook.com>
2016-08-04 02:39:14 +0000

    Added convnet basis

--- src/main/deep_naf_rl.py
+++ src/main/deep_naf_rl.py
@@ -10,27 +10,37 @@ reward_discount =.9
 actions = []
 R = [] # Initialize replay buffer R <- empty.
 m = 10 # minibatch size of transitions
+bias_size = 64
 
 # Randomly initialize normalized Q network Q(x, u|W_Q).
-w1 = tf.Variable (tf.random(), shape=())
-w2 = tf.Variable (tf.random(), shape=())
-b1 = tf.Variable (tf.random(), shape=())
-b2 = tf.Variable (tf.random(), shape=())
+x = tf.placeholder(tf.float32)
+xx = tf.reshape(x, [-1,28,28,1])
+
+# Conv layer 1
+w1 = tf.Variable (tf.truncated_normal(shape=[5,5,1,32], stddev=.1))
+b2 = tf.Variable (tf.const(.1, shape=[32]))
+conv1 = tf.nn.conv2d(xx, w1, stripes=[1,1,1,1], padding='SAME')
+activated_conv1 = tf.relu(conv1 + b1)
+pooled_conv1 = tf.nn.max_pool(activated_conv1, ksize = [1,2,2,1], strides = [1,1], padding='SAME') 
+
+# Conv layer 2
+w2 = tf.Variable (tf.truncated_normal(shape=[5,5,32,64], stddev=.1))
+b2 = tf.Variable (tf.const(.1, shape=[64]))
+conv2 = tf.nn.conv2d(pooled_conv1, w2, stripes=[1,1,1,1], padding='SAME')
+activated_conv2 = tf.relu(conv2 + b2)
+pooled_conv2 = tf.nn.max_pool(activated_conv2, ksize = [1,2,2,1], strides = [1,2,2,1], padding='SAME') 
 
 # Matrix of action-values (for each action) learned against a softmax of weight parameters, observation inputs and bias.
-a = tf.sigmoid (tf.matmul(w1, x) + b1)
-Qu = tf.softmax (tf.matmul(w2, a) + b2)
+w3 tf.Variable (tf.truncated_normal(shape=[7,7,64,1024], stddev=.1))
+b3 = tf.Variable (tf.const(.1, shape=[1024]))
+tf.matmul(pooled_conv2*w3) + b3
+Qu = tf.softmax (tf.matmul(pooled_conv2, w3) + b3)
 
 # Qu = Au + V
 
-# Initialize target network Q' with weight W_Q' <- W_Q.
-target_w1 = tf.Variable (tf.val(w1), shape=())
-target_w2 = tf.Variable (tf.val(w2), shape=())
-target_b1 = tf.Variable (tf.val(b1), shape=())
-target_b2 = tf.Variable (tf.val(b2), shape=())
 
-target_a = tf.sigmoid (tf.matmul(target_w1, x) + target_b1)
-target_Qu = tf.softmax (tf.matmul(target_w2, target_a) + target_b2)
+# Initialize target network Q' with weight W_Q' <- W_Q.
+# Empty!
 
 # target_Qu = target_Au + target_V
 
